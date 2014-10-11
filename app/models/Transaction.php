71d46ef9c7840b56971196789f218554ea2189fe
Andrey Popov <Nord001@gmail.com>
2014-10-11 18:43:45 +0400

    Связи для транзакции

--- app/models/Transaction.php
+++ app/models/Transaction.php
@@ -1,7 +1,23 @@
 <?php
 
-class Transaction extends \Eloquent {
+class Transaction extends \Eloquent
+{
     protected $table = 'transactions';
     public $timestamps = false;
-	protected $fillable = ['type_id', 'user_id', 'value'];
+    protected $fillable = ['type_id', 'item_id', 'user_id', 'value'];
+
+    public function type()
+    {
+        return $this->belongsTo('TransactionType', 'type_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('User', 'user_id');
+    }
+
+    public function item()
+    {
+        return $this->belongsTo('Item', 'item_id');
+    }
 }
