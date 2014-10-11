2ddb7b9049875fce977616914a4e400a383aced1
Andrey Popov <Nord001@gmail.com>
2014-10-12 01:57:43 +0400

    Личный кабинет

---
+++ app/views/cabinet/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+
+@section('content')
+
+    <h1>Личный кабинет</h1>
+
+    <a href="/cabinet/edit" class="btn btn-primary">
+        <span class="glyphicon glyphicon-plus"></span>
+        Редактировать
+    </a>
+@stop
